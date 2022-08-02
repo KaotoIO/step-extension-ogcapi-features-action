01bff0dfa3c6d8379914fa6c056c4b3ecbfea12c
kahboom <rachelyordan@gmail.com>
2022-08-02 10:12:58 +0100

    formatting

--- src/CollectionsDropDown.tsx
+++ src/CollectionsDropDown.tsx
@@ -1,38 +1,37 @@
 import * as React from 'react';
 
-export const CollectionsDropDown = ({ collections, callbackFunction, serverUrl, setCollection }) => {
-
+export const CollectionsDropDown = ({
+  collections,
+  callbackFunction,
+  serverUrl,
+  setCollection,
+}) => {
   const loadQueryable = (event) => {
-      setCollection(event.target.value);
-      let url = serverUrl + "/collections/" + event.target.value + "/queryables";
-      
-      fetch(url, {method: "GET", accept: "application/json"})
-          .then(response => response.json())
-          .then(data => {
-                callbackFunction(data.properties);
-              }
-          );
-    }
+    setCollection(event.target.value);
+    let url = serverUrl + '/collections/' + event.target.value + '/queryables';
 
-    return (
-      <>
-        <div>
-          <label>Collection:</label>
-          <select  className="form-control" name="collection" required onChange={loadQueryable}>
-            <option value="" >
-                Select a collection here
-            </option>
-            {collections.map((collection, idx) => {
-              return (
-                <option id={collection.id} value={collection.id} key={idx}>
-                    [{collection.id}] {collection.description.substring(0,45)} ...
-                </option>
-              )
-            })}
-          </select>
-        </div>
-      </>
-    );
+    fetch(url, { method: 'GET', accept: 'application/json' })
+      .then((response) => response.json())
+      .then((data) => {
+        callbackFunction(data.properties);
+      });
+  };
+
+  return (
+    <>
+      <div>
+        <label>Collection:</label>
+        <select className="form-control" name="collection" required onChange={loadQueryable}>
+          <option value="">Select a collection here</option>
+          {collections.map((collection, idx) => {
+            return (
+              <option id={collection.id} value={collection.id} key={idx}>
+                [{collection.id}] {collection.description.substring(0, 45)} ...
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </>
+  );
 };
-
-
